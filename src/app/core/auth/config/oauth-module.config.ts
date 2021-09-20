0c55daaef453d20851e2bdd99f84e6a97eead14f
Kyrillos Mamdoh <kyrillos.mamdoh@flairstech.com>
2021-09-20 12:30:38 +0200

    ngx-webstorage-service

--- src/app/core/auth/config/oauth-module.config.ts
+++ src/app/core/auth/config/oauth-module.config.ts
@@ -1,11 +1,11 @@
 import { RecoursesServerConfig } from 'src/app/config/servers.config';
-import { environment } from '@environments/environment';
-import { OAuthModuleConfig } from 'angular-oauth2-oidc';
+// import { OAuthModuleConfig } from 'angular-oauth2-oidc';
+import { environment } from 'src/environments/environment';
 
 
-export const authModuleConfig: OAuthModuleConfig = {
-  resourceServer: {
-    allowedUrls: [environment.apiUrl],
-    sendAccessToken: true,
-  }
-};
+// export const authModuleConfig: OAuthModuleConfig = {
+//   resourceServer: {
+//     allowedUrls: [environment.apiUrl],
+//     sendAccessToken: true,
+//   }
+// };
